3d2a0d00e579adcdd9e061db1dec28712e257f56
Arbzzz <arbaazjffr@gmail.com>
2023-06-04 16:01:26 +0100

    update 1

--- app.py
+++ app.py
@@ -22,19 +22,116 @@ def home():
 #  if it's a POST request process the submitted data
 #  but if it's a GET request, just show the form
 #------------------------------------------------------------
-@app.route('/new', methods = ['POST', 'GET'])
+@app.route('/new', methods = ['POST', 'GET']) 
 def create_buggy():
     if request.method == 'GET':
-        return render_template("buggy-form.html")
+        with sql.connect(DATABASE_FILE) as con:
+            cur = con.cursor()
+            cur.execute("SELECT qty_wheels, flag_color, flag_color_secondary, flag_pattern, power_type, tyres, armour, attack, algo, special, qty_tyres FROM buggies WHERE id=?", (DEFAULT_BUGGY_ID,))
+            result = cur.fetchone()
+            if result:
+                qty_wheels = result[0]
+                flag_color = result[1]
+                flag_color_secondary = result[2]
+                flag_pattern = result[3]
+                power_type = result[4]
+                tyres = result[5]
+                armour = result[6]
+                attack = result[7]
+                algo = result[8]
+                special = result[9]
+                qty_tyres = result[10]
+                return render_template("buggy-form.html", qty_wheels=qty_wheels, flag_color=flag_color, flag_color_secondary=flag_color_secondary, flag_pattern=flag_pattern, power_type=power_type, tyres=tyres, armour=armour, attack=attack, algo=algo, special=special, qty_tyres=qty_tyres)
+            else:
+                return render_template("buggy-form.html")
     elif request.method == 'POST':
         msg=""
         qty_wheels = request.form['qty_wheels']
+        qty_tyres = request.form['qty_tyres']
+        flag_color = request.form['flag_color']
+        flag_color_secondary = request.form['flag_color_secondary']
+        flag_pattern = request.form['flag_pattern']
+        power_type = request.form['power_type']
+        tyres = request.form['tyres']
+        armour = request.form['armour']
+        attack = request.form['attack']
+        algo = request.form['algo']
+        special = request.form['special']
+        flag_pattern = request.form['flag_pattern']
+
+        if not qty_wheels.isdigit() or not int(qty_wheels)%2 == 0 or int(qty_wheels) == 0:
+            qty_wheels_warning = "Quantity of wheels must be an even integer."
+            return render_template("buggy-form.html", qty_wheels_warning=qty_wheels_warning, qty_wheels=qty_wheels, qty_tyres=qty_tyres, flag_color=flag_color, flag_color_secondary=flag_color_secondary)
+
+        if not qty_tyres.isdigit() or int(qty_tyres) < int(qty_wheels):
+            qty_tyres_warning = "Quantity of tyres must be an integer and more or equal to the number of wheels."
+            return render_template("buggy-form.html", qty_tyres_warning=qty_tyres_warning, qty_wheels=qty_wheels, qty_tyres=qty_tyres, flag_color=flag_color, flag_color_secondary=flag_color_secondary)
+
+        power_cost = {
+            'bio': 5, 'electric': 20, 'fusion': 400, 'hamster': 3, 'none': 0,
+            'petrol': 4, 'rocket': 16, 'solar': 40, 'steam': 3, 'thermo': 300, 'wind': 20
+        }.get(power_type, 0)
+
+        tyres_cost = {
+            'knobbly': 15, 'maglev': 50, 'reactive': 40, 'slick': 10, 'steelband': 20
+        }.get(tyres, 0)
+
+        armour_cost = {
+            'aluminium': 200, 'none': 0, 'thicksteel': 200, 'thinsteel': 100, 'titanium': 290, 'wood': 40
+        }.get(armour, 0)
+
+        attack_cost = {
+            'biohazard': 30, 'charge': 28, 'flame': 20, 'none': 0, 'spike': 5
+        }.get(attack, 0)
+
+        special_cost = {
+            'antibiotic': 90, 'banging': 42, 'fireproof': 70, 'hamster booster': 5, 'insulated': 100
+        }.get(special, 0)
+
+        wheel_cost_percentage = ((int(qty_wheels) - 4) * 0.1) + 1
+        if wheel_cost_percentage > 1:
+            total_cost = power_cost + tyres_cost + (armour_cost*wheel_cost_percentage) + attack_cost + special_cost
+        elif wheel_cost_percentage <= 1:
+            total_cost = power_cost + tyres_cost + armour_cost + attack_cost + special_cost
+
+        colors = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "brown", "white", "black", "gray",
+                "cyan", "magenta", "lime", "teal", "indigo", "maroon", "navy", "olive", "silver", "gold", "turquoise",
+                "coral", "salmon", "ivory", "violet"]
+
+        if flag_color.lower() not in colors:
+            flag_color_warning = "Invalid flag color."
+            return render_template("buggy-form.html", flag_color_warning=flag_color_warning, qty_wheels=qty_wheels, qty_tyres=qty_tyres, flag_color=flag_color, flag_color_secondary=flag_color_secondary)
+        
+        if not flag_pattern == "plain" and flag_color.lower() == flag_color_secondary.lower():
+            color_combo_warning = "Invalid flag color combination."
+            return render_template("buggy-form.html", color_combo_warning=color_combo_warning)
+        
+        if flag_color_secondary.lower() not in colors:
+            flag_color_warning = "Invalid secondary flag color."
+            return render_template("buggy-form.html", flag_color_warning=flag_color_warning)
+        
+        if flag_pattern == "NA" or power_type == "NA" or tyres == "NA" or armour == "NA" or attack == "NA" or algo == "NA" or special == "NA":
+            Invalid_data_warning = "Must pick an option!"
+            return render_template("buggy-form.html", flag_pattern_warning=Invalid_data_warning if flag_pattern == "NA" else None,
+                                                    power_type_warning=Invalid_data_warning if power_type == "NA" else None,
+                                                    tyres_warning=Invalid_data_warning if tyres == "NA" else None,
+                                                    armour_warning=Invalid_data_warning if armour == "NA" else None,
+                                                    attack_warning=Invalid_data_warning if attack == "NA" else None,
+                                                    algo_warning=Invalid_data_warning if algo == "NA" else None,
+                                                    special_warning=Invalid_data_warning if special == "NA" else None,
+                                                    qty_wheels=qty_wheels, qty_tyres=qty_tyres,
+                                                    flag_color=flag_color, flag_color_secondary=flag_color_secondary)
+
         try:
+            qty_wheels = int(qty_wheels)
+            qty_tyres = int(qty_tyres)
+            flag_color = flag_color.upper()
+            flag_color_secondary = flag_color_secondary.upper()
             with sql.connect(DATABASE_FILE) as con:
                 cur = con.cursor()
                 cur.execute(
-                    "UPDATE buggies set qty_wheels=? WHERE id=?",
-                    (qty_wheels, DEFAULT_BUGGY_ID)
+                    "UPDATE buggies SET qty_wheels=?, qty_tyres=?, flag_color=?, flag_color_secondary=?, flag_pattern=?, power_type=?, tyres=?, armour=?, attack=?, special=?, total_cost=? WHERE id=?",
+                (qty_wheels, qty_tyres, flag_color, flag_color_secondary, flag_pattern, power_type, tyres, armour, attack, special, total_cost, DEFAULT_BUGGY_ID)
                 )
                 con.commit()
                 msg = "Record successfully saved"
@@ -64,8 +161,6 @@ def show_buggies():
 @app.route('/edit')
 def edit_buggy():
     return render_template("buggy-form.html")
-
-#------------------------------------------------------------
 # You probably don't need to edit this... unless you want to ;)
 #
 # get JSON from current record
@@ -83,6 +178,13 @@ def summary():
 
     buggies = dict(zip([column[0] for column in cur.description], cur.fetchone())).items() 
     return jsonify({ key: val for key, val in buggies if (val != "" and val is not None) })
+
+#------------------------------------------------------------
+# a page for displaying the component information
+#------------------------------------------------------------
+@app.route('/info')
+def get_info():
+    return render_template("info.html")
 
 # You shouldn't need to add anything below this!
 if __name__ == '__main__':
